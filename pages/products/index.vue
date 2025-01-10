20c7bb74a89872a0ed483b3e24f54ac9df01c2dd
ronssrony <ronssrony@gmail.com>
2025-01-10 16:14:18 +0600

    Add productCard component

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,21 +1,12 @@
 <template>
     <div class="translate-y-20 p-10  ">
-       <div class="w-52 h-72 relative p-4 shadow-md rounded flex flex-col justify-between">
-           <picture class="w-full h-1/2 relative">
-               <img class="rounded-md object-cover object-center " src="/assets/images/ll.jpg" alt="" srcset="">
-            </picture>
-            <div class="flex justify-between">
-              <span class="bg-blue-300/40  px-2 rounded-md text-sm text-blue-900 font-bebas  ">Up to 35% off</span>
-              <button> <Icon name="material-symbols:visibility-outline" class="text-black/60 " ></Icon> </button>
-              <button> <Icon name="ri:heart-line" class="text-black/60 "></Icon> </button>
-            </div>
-            <h1 class="font-medium line-clamp-2 ">Apple iMac 23" , 1TB HDD ,Retina 5K display, M3 Max</h1>
-            
-       </div>
+       
     </div>
 </template>
 
 <script setup >
+
+
 
 </script>
 
